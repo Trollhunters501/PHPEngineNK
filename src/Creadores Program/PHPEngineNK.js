be2ad5806d66f6c50ac83a93f89f429a3f1b99c1
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-01-28 23:42:26 -0600

    Create PHPEngineNK.js
    New

---
+++ src/Creadores Program/PHPEngineNK.js
@@ -0,0 +1,14 @@
+const PHPEngineNK = Class(Object, {
+  build: function(){
+    let libs = new NnClassLoader({ urls: [''] });
+    let subClass = {
+      put: function(name, content){
+        
+      },
+      eval: function(code){
+        
+      }
+    };
+    return subClass;
+  }
+});
