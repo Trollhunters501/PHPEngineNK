c414c1d64d91c663ed677379c7859e353bca04d1
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-01-31 21:49:19 -0600

    Update PHPEngineNK.js
    New libs

--- src/Creadores Program/PHPEngineNK.js
+++ src/Creadores Program/PHPEngineNK.js
@@ -1,6 +1,6 @@
 const PHPEngineNK = Class(Object, {
   build: function(){
-    let libs = new NnClassLoader({ urls: [''] });
+    let libs = new NnClassLoader({ urls: ['https://repo1.maven.org/maven2/com/caucho/quercus/4.0.66/quercus-4.0.66-javadoc.jar', 'https://repo1.maven.org/maven2/com/caucho/quercus/4.0.66/quercus-4.0.66-sources.jar', "https://repo1.maven.org/maven2/com/caucho/quercus/4.0.66/quercus-4.0.66.jar"] });
     let subClass = {
       put: function(name, content){
         
