b0296b01c9aaf5491ca7d5d5e140abeb4d7ce9ac
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-01-31 22:16:00 -0600

    Update PHPEngineNK.js
    New Code!

--- src/Creadores Program/PHPEngineNK.js
+++ src/Creadores Program/PHPEngineNK.js
@@ -1,12 +1,32 @@
 const PHPEngineNK = Class(Object, {
   build: function(){
     let libs = new NnClassLoader({ urls: ['https://repo1.maven.org/maven2/com/caucho/quercus/4.0.66/quercus-4.0.66-javadoc.jar', 'https://repo1.maven.org/maven2/com/caucho/quercus/4.0.66/quercus-4.0.66-sources.jar', "https://repo1.maven.org/maven2/com/caucho/quercus/4.0.66/quercus-4.0.66.jar"] });
+    let PHPFactor = libs.type("com.caucho.quercus.script.QuercusScriptEngineFactory");
+    let PHPManager = new PHPFactor();
+    let PHPEngine = PHPManager.getScriptEngine();
+    PHPEngine.put("server", server);
+    PHPEngine.put("getServer", server);
+    PHPEngine.put("getLogger", plugin.getLogger());
+    PHPEngine.put("logger", plugin.getLogger());
+    PHPEngine.put("plugin", plugin);
+    PHPEngine.put("manager", manager);
+    PHPEngine.put("players", players);
     let subClass = {
       put: function(name, content){
-        
+        PHPEngine.put(name, content);
       },
       eval: function(code){
-        
+        PHPEngine.eval(code);
+      },
+      getEngineName: function(){
+        return "PHPEngineNK &" + PHPManager.getEngineName();
+      },
+      setNnClassLoader: function(codeNC, classes){
+        let ClasLoad = new NnClassLoader(codeNC);
+        let KeysClass = Object.keys(classes);
+        for(var keys in KeysClass){
+          PHPEngine.put(KeysClass[keys], ClasLoad.type(classes[KeysClass[keys]]));
+        }
       }
     };
     return subClass;
